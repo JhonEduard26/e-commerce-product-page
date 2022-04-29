ce5b52b3dd20bc37e1ff2790100d92d50f506fa6
Jhon Eduard Bocanegra <jhon.bocanegra26@gmail.com>
2022-04-29 14:04:25 -0500

    [ADD] mobile layout and js

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,3 +27,47 @@ const closeMenu = () => {
 
 menuImg.addEventListener('click', showMenu)
 menuClose.addEventListener('click', closeMenu)
+
+/* product quantity */
+const minus = document.querySelector('.minus')
+const plus = document.querySelector('.plus')
+let productQuantity = document.querySelector('.product-quantity')
+let quantity = 1
+let productPriceAfter = document.querySelector('.product-price--after')
+let productPriceBefore = document.querySelector('.product-price--before')
+let productPrice = 125
+let newPrice
+
+const addProduct = () => {
+  quantity += 1
+  newPrice = productPrice * quantity
+  productPriceAfter.textContent = `$${newPrice}.00`
+  productPriceBefore.textContent = `$${newPrice * 2}.00`
+  productQuantity.textContent = quantity
+}
+
+const subtractProduct = () => {
+  if (quantity !== 1) {
+    quantity -= 1
+    newPrice -= productPrice
+  }
+  productPriceAfter.textContent = `$${newPrice || productPrice}.00`
+  productPriceBefore.textContent = `$${(newPrice || productPrice) * 2}.00`
+  productQuantity.textContent = quantity
+}
+
+plus.addEventListener('click', addProduct)
+minus.addEventListener('click', subtractProduct)
+
+/* Cart */
+const cartNumber = document.querySelector('.cart-number')
+const addToCart = document.querySelector('.add-to-cart')
+
+cartNumber.classList.add('hidden')
+
+const handleAddToCart = () => {
+  cartNumber.classList.remove('hidden')
+  cartNumber.textContent = quantity
+}
+
+addToCart.addEventListener('click', handleAddToCart)
