4e7524e4badebb45507a739bc4d0abeeb8967717
Jhon Eduard Bocanegra <jhon.bocanegra26@gmail.com>
2022-04-29 22:07:18 -0500

    [UPDATE] main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -62,12 +62,50 @@ minus.addEventListener('click', subtractProduct)
 /* Cart */
 const cartNumber = document.querySelector('.cart-number')
 const addToCart = document.querySelector('.add-to-cart')
+const productContent = document.querySelector('.product-content')
+const btnCheckout = document.querySelector('.checkout')
+const cartWarning = document.querySelector('.cart-warning__container')
+const productCost = document.querySelector('.product-price')
+const productCostFinal = document.querySelector('.product-price--final')
 
 cartNumber.classList.add('hidden')
 
 const handleAddToCart = () => {
   cartNumber.classList.remove('hidden')
+  productContent.classList.remove('hidden')
+  btnCheckout.classList.remove('hidden')
+  cartWarning.classList.add('hidden')
+  productCost.textContent = `$${productPrice}.00 x ${quantity}`
+  productCostFinal.textContent = `$${newPrice || productPrice}.00`
   cartNumber.textContent = quantity
 }
 
 addToCart.addEventListener('click', handleAddToCart)
+
+/* delete cart */
+const btnDelete = document.querySelector('.delete')
+
+const handleDelete = () => {
+  cartNumber.classList.add('hidden')
+  productContent.classList.add('hidden')
+  btnCheckout.classList.add('hidden')
+  cartWarning.classList.remove('hidden')
+}
+
+btnDelete.addEventListener('click', handleDelete)
+
+/* slider */
+const previous = document.querySelector('.previous')
+const next = document.querySelector('.next')
+const productGallerySlider = document.querySelector('.product-gallery__slider')
+const productImg = document.querySelector('.product-image')
+
+const handlePrevious = () => {
+  productGallerySlider.scrollLeft -= productImg.clientWidth
+}
+const handleNext = () => {
+  productGallerySlider.scrollLeft += productImg.clientWidth
+}
+
+previous.addEventListener('click', handlePrevious)
+next.addEventListener('click', handleNext)
